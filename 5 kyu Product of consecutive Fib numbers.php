b28d823bf21efafe8a75b67671f3f993d1fdcc87
AhmadOthman <trollingosman101@outlook.com>
2024-01-07 14:31:29 +0200

    easy peasy lemon squeezy

---
+++ 5 kyu Product of consecutive Fib numbers.php
@@ -0,0 +1,26 @@
+<?php
+
+function fib($n, &$memo = []) {
+    if ($n >= 0 && $n <= 2) return 1;
+    if (!empty($memo[$n])) return $memo[$n];
+    $memo[$n] = fib($n - 1, $memo) + fib($n - 2, $memo);
+    return $memo[$n];
+}
+
+function productFib($n) {
+    $res = [];
+    for ($i = 0; $i < $n; ++$i) {
+        $fib_i = fib($i);
+        $fib_j = fib($i + 1);
+        $product = ($fib_i * $fib_j);
+        if ($product == $n) return [$fib_i, $fib_j, true];
+        elseif ($product < $n) $res[0] = $fib_j;
+        elseif ($product > $n) {
+            $res[1] = $fib_j;
+            break;
+        }
+    }
+    return array_merge($res, [false]);
+}
+
+var_export(productFib(800));
