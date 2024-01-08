3a711bd8ddebf289a9d6470944259f42a26dfc89
Adam Ahmed <130517274+Frisky0A@users.noreply.github.com>
2024-01-08 07:42:20 -0800

    Update 6 kyu Highest Scoring Word.php

--- 6 kyu Highest Scoring Word.php
+++ 6 kyu Highest Scoring Word.php
@@ -10,3 +10,20 @@ function high($s) {
 }
 
 var_export(high('aa b'));
+
+// Another Solution by Frisky
+function high($x) {
+    $list = explode(" ", strtolower($x));
+    $maxword = ""; $maxval = 0;
+    foreach($list as $a){
+        $value = 0;
+        for($i = 0; $i < strlen($a); $i++){
+            $value += ord($a[$i]) - 96;
+        }
+        if($value > $maxval){
+            $maxval = $value;
+            $maxword = $a;
+        }
+    }
+    return $maxword;
+}
