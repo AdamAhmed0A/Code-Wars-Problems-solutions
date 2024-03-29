8b6932cce5a117445f4f93fb90758104c7e16815
AhmadOthman <trollingosman101@outlook.com>
2024-03-29 14:27:42 +0200

    feat: create '7 kyu Sum of two lowest positive integers.js'

---
+++ 7 kyu Sum of two lowest positive integers.js
@@ -0,0 +1,13 @@
+/* https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/javascript */
+
+/**
+ * 
+ * @param {Array} numbers 
+ * @param {any} memo 
+ */
+
+function sumTwoSmallestNumbers(numbers, memo = {}) {
+    if (numbers.length == 1) return
+
+    
+}
